c45b1e0eaad90f89a3840ce706435d46e3a399e0
jluna0321 <j.luna@outlook.com>
2017-08-30 19:01:37 -0400

    Update testjsonagain.js

--- testjsonagain.js
+++ testjsonagain.js
@@ -1,40 +1,41 @@
 /* <sl:translate_generic_json> */
 var tmplCobrandData = { sl_translate: "text,heading,content,title",
-    sl_suffix: "Text",
-    
-        cobrand: {
-            headlineText: "<a href='/eligibilitypremiumcalc' title='Go to Eligibility & Premium THISISATEST Homepage'><img src='img/cobrand_eligibility_calculator.png' alt='Eligibility & Premium THISISATEST' href='/medicareeligibilitytool/index.html' /></a>",
-            hideMenu: false,
-            ariaHeadingText: "Main Menu",
-            itemsToHide: ".toolbarnavli, #espanolli",
-            itemsToHideStatic: "",
-            newTopMenuItems: [
-                { text: "THISISATEST.gov", href: "//www.THISISATEST.gov", className: "toolbarnavli", title: "Go to THISISATEST.gov", index: 3, siteliIndex: 1 }
-            ],
-            newStaticTopMenuItems: [
-                { text: "THISISATEST.gov", href: "//www.THISISATEST.gov", className: "toolbarnavli", title: "Go to THISISATEST.gov" },
-                { text: "Close Window", href: "javascript:window.close();", className: "toolbarnavli", title: "Close window" }
-            ],
-            menuText: "Menu",
-            menuItems: [
-                { text: "Eligibility & Premium THISISATEST Home", href: "/eligibilitypremiumcalc", target: "", title: "Eligibility & Premium THISISATEST Home" }
-    
-           ],
-           staticMenuItems: [
-                { text: "Eligibility & Premium THISISATEST Home", href: "/eligibilitypremiumcalc", target: "", title: "Eligibility & Premium THISISATEST Home" }
+sl_suffix: "Text",
+sl_html: "text",
 
-           ],
-    
-            breadcrumb:
-           {
-               searchUrl: "/eligibilitypremiumcalc",
-               searchText: "Home"
-           },
-            messageUpdates:
-           {
-               text: "This is an update to the Eligibility and Premium THISISATEST Page"
-           }
-    
-        }
+    cobrand: {
+        headlineText: "<a href='/eligibilitypremiumcalc' title='Go to Eligibility & Premium THISISATEST Homepage'><img src='img/cobrand_eligibility_calculator.png' alt='Eligibility & Premium THISISATEST' href='/medicareeligibilitytool/index.html' /></a>",
+        hideMenu: false,
+        ariaHeadingText: "Main Menu",
+        itemsToHide: ".toolbarnavli, #espanolli",
+        itemsToHideStatic: "",
+        newTopMenuItems: [
+            { text: "THISISATEST.gov", href: "//www.THISISATEST.gov", className: "toolbarnavli", title: "Go to THISISATEST.gov", index: 3, siteliIndex: 1 }
+        ],
+        newStaticTopMenuItems: [
+            { text: "THISISATEST.gov", href: "//www.THISISATEST.gov", className: "toolbarnavli", title: "Go to THISISATEST.gov" },
+            { text: "Close Window", href: "javascript:window.close();", className: "toolbarnavli", title: "Close window" }
+        ],
+        menuText: "Menu",
+        menuItems: [
+            { text: "Eligibility & Premium THISISATEST Home", href: "/eligibilitypremiumcalc", target: "", title: "Eligibility & Premium THISISATEST Home" }
+
+       ],
+       staticMenuItems: [
+            { text: "Eligibility & Premium THISISATEST Home", href: "/eligibilitypremiumcalc", target: "", title: "Eligibility & Premium THISISATEST Home" }
+
+       ],
+
+        breadcrumb:
+       {
+           searchUrl: "/eligibilitypremiumcalc",
+           searchText: "Home"
+       },
+        messageUpdates:
+       {
+           text: "This is an update to the Eligibility and Premium THISISATEST Page"
+       }
+
     }
+}
 /* </sl:translate_generic_json> */
