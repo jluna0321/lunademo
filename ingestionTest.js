f834956cac51dbc49fcb644979b7f744e0bc02fc
jluna0321 <j.luna@outlook.com>
2018-11-14 13:40:38 -0500

    Create ingestionTest.js

---
+++ ingestionTest.js
@@ -0,0 +1,17 @@
+define({
+    '/Content/Modeling/Build Models/Actions/Other_Actions.html': {
+        i: [89],
+        t: ['This one 135 should come in'],
+        b: ['']
+    },
+    '/Content/Modeling/Build Models/Actions/Other_Actions/Assign.html': {
+        i: [90],
+        t: ['Let's bring 1059 this one in too'],
+        b: ['']
+    },
+    '/Content/Modeling/Build Models/Actions/Other_Actions/Copy_Branch.html': {
+        i: [91],
+        t: ['And another 1921312 one here please.'],
+        b: ['']
+    },
+});
