e1684a82a63eab8874fe80d7f8c6a937cca9d57e
jluna0321 <j.luna@outlook.com>
2018-11-14 18:03:59 -0500

    Update ingestionTest.js

--- ingestionTest.js
+++ ingestionTest.js
@@ -8,12 +8,16 @@ define({
     },
     '/Content/TESTPATH/Assign.html': {
         i: [90],
+        sl_tr_start();,
         t: ['Let us bring 32421 this one in too'],
+        sl_tr_end();,
         b: ['']
     },
     '/Content/TESTPATH/Copy_Branch.html': {
         i: [91],
+        sl_tr_start();,
         t: ['And another 1921312 one here please.'],
+        sl_tr_end();,
         b: ['']
     },
 });
